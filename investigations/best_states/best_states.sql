eeec6ad8766c812fa4df38970b3d1587d89fc68b
ozimmer <zimmer.olivier@gmail.com>
2016-06-19 12:27:36 -0400

    update investigations file

--- investigations/best_states/best_states.sql
+++ investigations/best_states/best_states.sql
@@ -1,4 +1,6 @@
 -- States being models of high-quality care
+drop table if exists best_states;
+create table best_states as
 select
 state,
 sum(score)/count(score) as score
@@ -6,3 +8,5 @@ from provides
 group by state
 order by score DESC
 ;
+
+select state, score from best_states limit 10;
--- investigations/hospital_variability/hospital_variability.sql
+++ investigations/hospital_variability/hospital_variability.sql
@@ -1,11 +1,14 @@
 -- Variability of procedures
+drop table if exists hospital_variability;
+create table hospital_variability as
 select
 m.measure_name as measure_name,
 stddev_pop(score) as standard_deviation
---sum(power((score - sum(score)/count(score)), 2))/(count(score) - 1) as variance
 from procedures as p
 join measures as m
 on m.measure_id = p.measure_id
 group by measure_name
 order by standard_deviation DESC
 ;
+
+select measure_name, standard_deviation from hospital_variability limit 10;
